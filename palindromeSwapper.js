be71718a0fc6fd6eefb5ace7afce63e316045d56
mario <panggihmario@gmail.com>
2018-05-01 15:10:37 +0700

    broken-palindrom

--- palindromeSwapper.js
+++ palindromeSwapper.js
@@ -6,20 +6,26 @@
  * @returns {boolean} true bila kata yang di-swap adalah palindrom
  */
 function palindromeSwapper(str) {
-  let newStr = '';
+  
+  // var tampung=[]
   for (var i = 0; i < str.length; i++) {
-    for (var j = 0; j < str.length; i++) {
-      if (i === j) {
-        newStr += str[j++];
+    let newStr = '';
+    for (var j = 0; j < str.length; j++) {
+      if (i === j && j < str.length -1) {
+        newStr += str[j+1];
         newStr += str[j];
         j++;
       } else {
         newStr += str[j];
       }
     }
+    
     if (isPalindrome(newStr)) return true;
+    // tampung.push(newStr)
   }
   return false;
+  // return tampung
+
 }
 
 /**
@@ -29,8 +35,9 @@ function palindromeSwapper(str) {
  * @returns {boolean} true bila kata adalah palindrom
  */
 function isPalindrome(str) {
-  if (str.split('').reverse().join() === str) return console.log(true);
-  return console.log(false);
+  if (str.split('').reverse().join('') === str) return true;
+  
+  return false;
 }
 
 console.log(palindromeSwapper('arcecar')); // TRUE
