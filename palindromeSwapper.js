c0788f2eb97d23a854f2d882a48a48d52a51da57
adrowicaksono <adrowicaksono@gmail.com>
2018-05-01 10:51:47 +0700

    fixing function of  palindroneSwapper

--- palindromeSwapper.js
+++ palindromeSwapper.js
@@ -6,18 +6,24 @@
  * @returns {boolean} true bila kata yang di-swap adalah palindrom
  */
 function palindromeSwapper(str) {
+  
+  
   let newStr = '';
   for (var i = 0; i < str.length; i++) {
-    for (var j = 0; j < str.length; i++) {
+    newStr = '';
+    for (var j = 0; j < str.length; j++) {
       if (i === j) {
-        newStr += str[j++];
+        newStr += str[j+1];
         newStr += str[j];
         j++;
       } else {
         newStr += str[j];
       }
+      debugger;
     }
-    if (isPalindrome(newStr)) return true;
+        if (isPalindrome(newStr) || isPalindrome(str)) {
+          return true;
+        }    
   }
   return false;
 }
@@ -29,8 +35,12 @@ function palindromeSwapper(str) {
  * @returns {boolean} true bila kata adalah palindrom
  */
 function isPalindrome(str) {
-  if (str.split('').reverse().join() === str) return console.log(true);
-  return console.log(false);
+  if (str.split('').reverse().join('') === str) {
+    return true;
+  }else{
+    
+  return false;
+  }
 }
 
 console.log(palindromeSwapper('arcecar')); // TRUE
