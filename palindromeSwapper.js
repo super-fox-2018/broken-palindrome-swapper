2f1e3136881ed62a49fedec816c7bb0285c26af3
alprak <you@example.com>
2018-05-01 15:32:44 +0700

    done

--- palindromeSwapper.js
+++ palindromeSwapper.js
@@ -5,18 +5,23 @@
  * @param {string} str - input kata yang ingin di-swap dan dicek palindrom
  * @returns {boolean} true bila kata yang di-swap adalah palindrom
  */
+
+ //'arcecar'
 function palindromeSwapper(str) {
   let newStr = '';
   for (var i = 0; i < str.length; i++) {
-    for (var j = 0; j < str.length; i++) {
+    for (var j = 0; j < str.length; j++) {
       if (i === j) {
-        newStr += str[j++];
+        //console.log("====",newStr)
+        newStr += str[j+1];
         newStr += str[j];
         j++;
       } else {
+        //console.log("xxxx",newStr)
         newStr += str[j];
       }
     }
+    //console.log('xxxx',newStr)
     if (isPalindrome(newStr)) return true;
   }
   return false;
@@ -29,8 +34,8 @@ function palindromeSwapper(str) {
  * @returns {boolean} true bila kata adalah palindrom
  */
 function isPalindrome(str) {
-  if (str.split('').reverse().join() === str) return console.log(true);
-  return console.log(false);
+  if (str.split('').reverse().join("") === str) return (true);
+  return (false);
 }
 
 console.log(palindromeSwapper('arcecar')); // TRUE
